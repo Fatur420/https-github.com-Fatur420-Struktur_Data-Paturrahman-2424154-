44928d27a01ba0a786a75c98242b5fbee2219a75
Fatur420 <akufatur36@gmail.com>
2025-05-16 11:32:58 +0800

    Create Meet2.py

---
+++ Meet2.py
@@ -0,0 +1,30 @@
+# operasi Logika
+# or and xor not
+
+# Operasi NOT
+x = True
+z = not x
+print("nilai dari z =", z)
+
+print("********** AND **********")
+
+# Operasi AND
+x = True
+y = True
+z = x and y
+print(x, 'and', y, '=', z)
+
+x = True
+y = False
+z = x and y
+print(x, 'and', y, '=', z)
+
+x = False
+y = True
+z = x and y
+print(x, 'and', y, '=', z)
+
+x = False
+y = False
+z = x and y
+print(x, 'and', y, '=', z)
